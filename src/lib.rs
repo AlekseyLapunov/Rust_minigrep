0fba0ca8940642c15a06ee271645c33c28151dc1
Aleksei Lapunov <lapunoff.alex@yandex.ru>
2024-08-04 17:04:22 +0700

    Add tests and implement search method

--- src/lib.rs
+++ src/lib.rs
@@ -20,7 +20,67 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    println!("Contents:\n{contents}");
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
 
     Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn multiple_results() {
+        let query = "t";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["Rust:", "safe, fast, productive.", "Pick three."], search(query, contents));
+    }
+
+    #[test]
+    fn no_results() {
+        let query = "UFO";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert!(search(query, contents).is_empty());
+    }
+
+    #[test]
+    fn empty_contents() {
+        let query = "UFO";
+        let contents = "";
+
+        assert!(search(query, contents).is_empty());
+    }
 }
